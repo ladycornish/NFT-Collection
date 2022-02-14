919925de34f056dabca363f3802c154e1994d261
Mel <melcornish@me.com>
2022-02-14 11:54:18 +0000

    add api

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xd9c0F2a232C3AA5D0bF69Bdb676aa43409f6aCA0";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-take3.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-take3-ladycornish.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
